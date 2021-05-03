ff37354bf4125ec4f5df198cdcc62abb3d506c25
HSA-Student <student.s.sow@horizoncolubus.org>
2021-05-03 08:09:55 -0400

    Project9

---
+++ student.py
@@ -0,0 +1,8 @@
+from user import user
+class Studen(User):
+    result = 0
+    def type(self):
+        pass
+
+    def display(self):
+        pass
---
+++ user.py
@@ -0,0 +1,9 @@
+class User:
+    name = ""
+    user_name = ""
+    access_level = ""
+
+    def ___init___(self, new_name, new_username, new_access_lebel):
+        self.name = new_name
+        self.username = new_username
+        self.access_level = new_access_level
